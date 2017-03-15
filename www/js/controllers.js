0535d7f0be33b99797f060bf042142b56f2ef7f1
MeganMFisher <meganmichelle318@gmail.com>
2017-03-15 12:09:50 -0600

    changed home view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,73 +8,51 @@ function ($scope, $stateParams, BlankService) {
 
 }])
    
-.controller('donateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('donateCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 
 }])
    
-.controller('page3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('page3Ctrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
 
 }])
    
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('menuCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 
 }])
    
-.controller('shopCtrl', ['$scope', '$stateParams', 'BlankService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('shopCtrl', ['$scope', '$stateParams', 'BlankService', 
 function ($scope, $stateParams, BlankService) {
 
-        // $scope.recProducts = function(){
       BlankService.getAllProducts().then(function(resp) {
         $scope.products = resp.data;
       });
-    // }
-
-    // $scope.recProducts();
 
 
 }])
    
-.controller('socialMediaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('socialMediaCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 
 }])
    
-.controller('ProductInfoCtrl', ['$scope', '$stateParams', 'BlankService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('ProductInfoCtrl', ['$scope', '$stateParams', 'BlankService', 
 function ($scope, $stateParams, BlankService) {
 
-    // $scope.recProduct = function(id)'
       BlankService.getOneProduct($stateParams.id).then(function(resp){
         $scope.detail = resp.data[0];
-        console.log($scope.detail)
       })
-    // }
 
-    // $scope.recProduct($stateParams.id);
 
 }])
    
-.controller('shoppingCartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('shoppingCartCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 
--- www/js/services.js
+++ www/js/services.js
@@ -21,6 +21,65 @@ angular.module('app.services', [])
       })
     }
 
+    this.getUsers = function() {
+			return $http({
+				method: 'GET',
+				url: '/user'
+			});
+		};
+
+		this.getUserOrder = function(id) {
+			return $http({
+					method: 'GET',
+					url: '/order/' + id
+				})
+				.catch(function(err) {
+					console.log(err);
+
+				});
+		};
+
+		this.getProducts = function() {
+			return $http({
+				method: "GET",
+				url: "/products"
+			});
+		};
+
+		this.addToCart = function(id, productid, qty) {
+			return $http({
+				method: 'POST',
+				url: '/add/item/cart/' + id,
+				data: {
+					id: productid,
+					qty: qty
+				}
+			});
+		};
+
+		this.updateProductQty = function(id, qty) {
+			return $http({
+				method: 'PUT',
+				url: "/update/qty/" + id,
+				data: {
+					qty: qty
+				}
+			});
+		};
+
+		this.removeFromCart = function(id) {
+			return $http({
+				method: 'DELETE',
+				url: '/delete/item/cart/' + id
+			});
+		};
+
+		this.placeOrder = function(id, orderid) {
+			return $http({
+				method: 'PUT',
+				url: '/order/complete/' + orderid + "/" + id
+			});
+		};
   
 
 
