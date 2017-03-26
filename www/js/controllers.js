1dcdada3411705efee0ffb50465ab5ddb8e840da
MeganMFisher <meganmichelle318@gmail.com>
2017-03-25 23:38:15 -0600

    updated animations

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,9 +4,7 @@ angular.module('app.controllers', [])
   // *                homeCtrl                 *
   // *******************************************
 
-  .controller('homeCtrl', ['$scope', '$stateParams', 'BlankService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-    // You can include any angular dependencies as parameters for this function
-    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+  .controller('homeCtrl', ['$scope', '$stateParams', 'BlankService', 
     function ($scope, $stateParams, BlankService) {
 
     }
